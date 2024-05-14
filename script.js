da15cb51e13041e3ea53ec6bb64b51de4bd70f92
Jimi <98427565+jimididit@users.noreply.github.com>
2024-05-13 20:35:57 -0400

    todo: Add error handling, filenaming, view toggle, UI customization

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TikTok BlockList Creator & AutoBlocker :)
+// @name         tiktok-autoblocker
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Collect TikTok usernames to block and download them as a .txt file.
@@ -12,51 +12,129 @@
 (function() {
     'use strict';
 
-    const blockListKey = 'tiktokAutoBlockList';
+    const blockListKey = 'tiktokBlockList';
     console.log('Running Autoblock and Blocklist Creator Script');
-
     checkForPostNavigationTask();
 
     function checkForPostNavigationTask() {
         const task = JSON.parse(localStorage.getItem('autoBlock'));
-        if (task && window.location.href.includes(`https://www.tiktok.com/${task.username}`)) {
-            performBlockOperation(task.username);
+        if (task) {
+            performBlockOperation(task);
+        }
+    }
+
+    async function performBlockOperation(task) {
+        if (!window.location.href.includes(`https://www.tiktok.com/${task.username}`)) {
+            window.location.href = `https://www.tiktok.com/${task.username}`;
+            return;
+        }
+
+        console.log('User Blocking Init');
+
+        const moreButton = await waitForElement('[data-e2e="user-more"]', 5000);
+        if (moreButton) {
+            simulateMouseEvent(moreButton, 'mouseover');
+            console.info('User-more button found, showing more options...');
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const blockButton = await waitForElement('[aria-label="Block"]', 1000);
+            if (blockButton) {
+                simulateMouseEvent(blockButton, 'click');
+                console.info('Block button clicked...');
+                await new Promise(resolve => setTimeout(resolve, 1000));
+
+                const confirmButton = await waitForElement('button[data-e2e="block-popup-block-btn"]', 1000);
+                if (confirmButton) {
+                    simulateMouseEvent(confirmButton, 'click');
+                    console.info('Block confirmed...');
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+            }
+        } else {
+            console.error('No more options button found!!')
+        }
+        handleNextUser();
+    }
+
+    function handleNextUser() {
+        const users = JSON.parse(localStorage.getItem('autoBlockQueue') || '[]');
+        if (users.length > 0) {
+            const nextUser = users.shift();
+            localStorage.setItem('autoBlockQueue', JSON.stringify(users));
+            localStorage.setItem('autoBlock', JSON.stringify(nextUser));
+            checkForPostNavigationTask();
+        } else {
+            console.log('No more users in the queue.');
+            localStorage.removeItem('autoBlockQueue');
             localStorage.removeItem('autoBlock');
         }
     }
 
-    async function performBlockOperation(username) {
-        console.log('User Blocking Init');
-
-        const moreButton = await waitForElement('[data-e2e="user-more"]', 10000);
-        if (moreButton) {
-            simulateMouseEvent(moreButton, 'mouseover');
-            console.info('Got user-more...');
-            await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for UI to update
-
-            const blockButton = await waitForElement('[aria-label="Block"]', 1000);
-            if (blockButton) {
-                simulateMouseEvent(blockButton, 'click'); // Click to block the user
-                await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for modal to appear
-                console.info('Got First Block Button...');
-                const confirmButton = await waitForElement('#tux-portal-container button[data-e2e="block-popup-block-btn"]', 1000);
-                if (confirmButton) {
-                    simulateMouseEvent(confirmButton, 'click'); // Confirm blocking the user
-                    console.info('Got Block Confirmation Button...');
-                    await new Promise(resolve => setTimeout(resolve, 3000)); // Wait for the block to be processed
+    function waitForElement(selector, timeout) {
+        return new Promise((resolve, reject) => {
+            const intervalTime = 100;
+            const endTime = Number(new Date()) + timeout;
+            const timer = setInterval(() => {
+                if (Number(new Date()) > endTime) {
+                    clearInterval(timer);
+                    reject(new Error("Element not found within time: " + selector));
                 }
-            }
-        } else {
-            console.error('No user-more button found!!')
-        }
+                const el = document.querySelector(selector);
+                if (el) {
+                    clearInterval(timer);
+                    resolve(el);
+                }
+            }, intervalTime);
+        });
+    }
+
+    function simulateMouseEvent(element, eventType) {
+        console.log(`Simulating ${eventType} event`);
+        const event = new MouseEvent(eventType, {
+            view: window,
+            bubbles: true,
+            cancelable: true
+        });
+        element.dispatchEvent(event);
+        console.log(`${eventType} event triggered`);
+    }
+
+    function init() {
+        const card = createCard('Block List Manager');
+        addButton(card, 'Add User to Block List', addUserToBlockList);
+        addButton(card, 'Download Block List', downloadBlockList);
+        // const filenameInput = createFilenameInput(card);
+        createFileInput(card);
+    }
+
+    function addButtonFunctionality(card) {
+        addButton(card, 'Add to Block List', addUserToBlockList);
+        addButton(card, 'Download Block List', downloadBlockList);
+    }
+
+    function createFileInput(card) {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.style.width = '100%';
+        fileInput.style.marginTop = '10px';
+        fileInput.onchange = handleFileUpload;
+        card.appendChild(fileInput);
+    }
+
+    async function handleFileUpload(event) {
+        const file = event.target.files[0];
+        const text = await file.text();
+        const usernames = text.split(/\r?\n/).filter(u => u.trim() !== '').map(username => ({username: username.trim(), action: 'block'}));
+        localStorage.setItem('autoBlockQueue', JSON.stringify(usernames));
+        handleNextUser();
     }
 
     function createCard(cardTitle) {
         const card = document.createElement('div');
         card.style.position = 'fixed';
-        card.style.top = '175px';
+        card.style.top = '150px';
         card.style.right = '20px';
-        card.style.width = '200px';
+        card.style.width = '250px';
         card.style.backgroundColor = '#fff';
         card.style.boxShadow = '0 4px 8px rgba(0,0,0,0.1)';
         card.style.padding = '10px';
@@ -89,39 +167,26 @@
         card.appendChild(button);
     }
 
-    function createFileInput(card) {
-        const fileInput = document.createElement('input');
-        fileInput.type = 'file';
-        fileInput.style.width = '100%';
-        fileInput.style.marginTop = '10px';
-        fileInput.onchange = handleFileUpload;
-        card.appendChild(fileInput);
+    function createFilenameInput(card) {
+        // Divider
+        const divider = document.createElement('hr');
+        divider.style.marginTop = '10px';
+        divider.style.marginBottom = '10px';
+        card.appendChild(divider);
+
+        const filenameLabel = document.createElement('label');
+        filenameLabel.textContent = 'Blocklist name:';
+        filenameLabel.style.display = 'block';
+        filenameLabel.style.marginBottom = '5px';
+        filenameLabel.style.marginTop = '5px';
+        filenameLabel.style.color = '#333';
+        card.appendChild(filenameLabel);
     }
 
-    async function handleFileUpload(event) {
-        const file = event.target.files[0];
-        if (!file) return;
-
-        const text = await file.text();
-        const usernames = text.split(/\r?\n/);
-        for (let username of usernames) {
-            if (username.trim()) {
-                localStorage.setItem('autoBlock', JSON.stringify({username: username.trim(), action: 'block'}));
-                window.location.href = `https://www.tiktok.com/${username.trim()}`;
-                break; // Break after setting up the first redirection
-            }
-        }
-    }
-
-    function addButtonFunctionality(card) {
-        addButton(card, 'Add to Block List', addUserToBlockList);
-        addButton(card, 'Download Block List', downloadBlockList);
-    }
 
     function addUserToBlockList() {
         const username = window.location.pathname.split('/')[1];
         const blockList = JSON.parse(localStorage.getItem(blockListKey) || '[]');
-
         if (!blockList.includes(username)) {
             blockList.push(username);
             localStorage.setItem(blockListKey, JSON.stringify(blockList));
@@ -133,52 +198,16 @@
 
     function downloadBlockList() {
         const blockList = JSON.parse(localStorage.getItem(blockListKey) || '[]');
+        //const filename = filenameInput.value.trim();
         const blob = new Blob([blockList.join('\n')], { type: 'text/plain;charset=utf-8' });
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
-        a.download = 'tiktok-block-list.txt';
+        a.download = `tiktok-blocklist.txt`;
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
         URL.revokeObjectURL(url);
-    }
-
-    function waitForElement(selector, timeout) {
-        return new Promise((resolve, reject) => {
-            const intervalTime = 100; // Check every 100 ms
-            const endTime = Number(new Date()) + timeout;
-
-            const timer = setInterval(() => {
-                if (Number(new Date()) > endTime) {
-                    clearInterval(timer);
-                    reject(new Error("Element not found within time: " + selector));
-                }
-
-                const el = document.querySelector(selector);
-                if (el) {
-                    clearInterval(timer);
-                    resolve(el);
-                }
-            }, intervalTime);
-        });
-    }
-
-    function simulateMouseEvent(element, eventType) {
-        console.log('Simulating Hover Event');
-        const mouseOverEvent = new MouseEvent(`${eventType}`, {
-            view: window,
-            bubbles: true,
-            cancelable: true
-        });
-        element.dispatchEvent(mouseOverEvent);
-        console.log(`${eventType.toUpperCase()} Complete: ${mouseOverEvent}`);
-    }
-
-    function init() {
-        const card = createCard('Let <em><b>Them</b></em> Eat Shit!');
-        addButtonFunctionality(card);
-        createFileInput(card);
     }
 
     init();
