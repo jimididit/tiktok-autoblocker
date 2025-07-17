ee9053fc22398b72c91659b52d60facb63de78b2
jimididit <jimi@jimididit.com>
2025-07-16 21:50:24 -0400

    Update version to v0.3.0

--- tampermonkey/script.js
+++ tampermonkey/script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tiktok-autoblocker
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.3.0
 // @description  Collect TikTok usernames to block and download them as a .txt file. Enhanced with private account support and improved blocking sequence.
 // @author       jimididit
 // @match        *://*.tiktok.com/*
